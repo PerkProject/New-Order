c1167c234601b69d9726c12426acc9a1acbb8aa5
Nick Demyanchuk <spike@scalr.com>
2015-02-20 01:28:53 +0300

    Optional filepath argument added, map became lazy

--- sort.py
+++ sort.py
@@ -1,6 +1,9 @@
 __author__ = "Nick Demyanchuk"
 
+import sys
+
 import logging
+import itertools
 
 logging.basicConfig()
 logger = logging.getLogger()
@@ -8,10 +11,10 @@ logger.setLevel(logging.DEBUG)
 
 ALLOWED_CHARS = ("1", "2", "X")
 
-def main():
+def main(filename):
     already_processed = list()
 
-    with open("2748373.txt") as f:
+    with open(filepath) as f:
         with open('output.txt', 'w') as result_fp:
             for line in f:
                 # Get latest element - our digits set
@@ -20,20 +23,16 @@ def main():
                 maybe_number = maybe_number.replace(';', '')
 
                 if not len(maybe_number) == 14:
-                    logger.info("Number length of {} is not 14 symbols".format(maybe_number))
                     continue
 
-                if not all(map(lambda n: n in ALLOWED_CHARS, maybe_number)):
+                if any(itertools.imap(lambda n: n not in ALLOWED_CHARS, maybe_number)):
                     # Unknown character detected
-                    logger.info("Unknown characters detected in {}".format(maybe_number))
                     continue
 
                 if not maybe_number in already_processed:
                     already_processed.append(maybe_number)
                     result_fp.write(maybe_number + "\n")
-                else:
-                    logger.info('{} has already been processed'.format(maybe_number))
-
 
 if __name__ == "__main__":
-    main()
+    filepath = len(sys.argv) > 1 and sys.argv[1] or "2748373.txt"
+    main(filepath)
